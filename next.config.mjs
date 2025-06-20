2826f36cc482c0f77ef4935a173ba2ff3f366f2f
Md Mofazzal Hossain <213902114@student.green.edu.bd>
2025-06-21 05:53:48 +0600

    fixed nextra problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import nextra from "nextra";
+
 const nextConfig = {};
 
-export default nextConfig;
+const withNextra = nextra({});
+
+export default withNextra({
+  nextConfig,
+});
